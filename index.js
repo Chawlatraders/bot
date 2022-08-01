6a312276d499924e48a02a71b8ee70ded1fadffb
Prashant Chawla <devchawla.dc@gmail.com>
2022-08-02 01:39:11 +0530

    CUSTOM X,Y COORDINATES FOR COMMIT

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const FILE_PATH="./data.json";
 const moment = require('moment');
 const simpleGit=require('simple-git')
 
+const makeCommit=(x,y)=> {
 
 
-
-const DATE=moment().subtract(1,'d').format();
+const DATE=moment().subtract(1,'y').add(1,'d')
+                    // add(x,'w').add(y,'d').format(); add x weeks and y days
+                    .add(x,'w').add(y,'d').format();
 const data={
     date:DATE
 }
@@ -15,4 +17,5 @@ jsonfile.writeFile(FILE_PATH,data);
 
 
 simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE})
-
+}
+makeCommit(0,4);
